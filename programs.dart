0b32934aa311b006ef4fdfd18170837db6e754bc
sandhyaNepal20 <nsandhya192@gmail.com>
2024-11-18 21:22:54 +0545

    program 4 commited

--- programs.dart
+++ programs.dart
@@ -14,15 +14,29 @@
 //   }
 // }
 
-void main() {
-  int num1 = 12;
-  int num2 = 5;
+//find the largest num
+// void main() {
+//   int num1 = 12;
+//   int num2 = 5;
 
-  if (num1 > num2) {
-    print("$num1 is larger than $num2");
-  } else if (num1 < num2) {
-    print("$num2 is larger than $num1");
+//   if (num1 > num2) {
+//     print("$num1 is larger than $num2");
+//   } else if (num1 < num2) {
+//     print("$num2 is larger than $num1");
+//   } else {
+//     print("$num1 and $num2 are equal");
+//   }
+// }
+
+//checking numbers +ve, -ve ...and zero
+void main() {
+  int number = 0;
+
+  if (number > 0) {
+    print("$number is positive");
+  } else if (number < 0) {
+    print("$number is negative");
   } else {
-    print("$num1 and $num2 are equal");
+    print("The number is zero");
   }
 }
