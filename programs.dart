bf7e5e812070558fec6b49c6c7d421782b3effd7
sandhyaNepal20 <nsandhya192@gmail.com>
2024-11-18 21:16:20 +0545

    program 2 commited

--- programs.dart
+++ programs.dart
@@ -1,8 +1,15 @@
 //sum of two numbers
-void main() {
-  int num1 = 5;
-  int num2 = 9;
-  int sum = num1 + num2;
+// void main() {
+//   int num1 = 5;
+//   int num2 = 9;
+//   int sum = num1 + num2;
 
-  print("The sum of $num1 and $num2 is $sum");
+//   print("The sum of $num1 and $num2 is $sum");
+// }
+
+//print 1 to 10
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
 }
